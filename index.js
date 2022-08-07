ed0c2b9ff32a3b7e54244c3d8288dc66d00828fc
Lauren Metz <Laurenmetz15@gmail.com>
2022-08-07 16:33:14 -0400

    commit tree 9d4605291f0ddda45aee02c9766ceab114035f6f

--- index.js
+++ index.js
@@ -1,5 +1,77 @@
 /* Your Code Here */
 
+function createEmployeeRecord(info) {
+    let newEmployee = {};
+    newEmployee.firstName = info[0];
+    newEmployee.familyName = info[1];
+    newEmployee.title = info[2];
+    newEmployee.payPerHour = info[3];
+    newEmployee.timeInEvents = [];
+    newEmployee.timeOutEvents = [];
+    return newEmployee;
+}
+
+function createEmployeeRecords(employees) {
+    let records = employees.map(employee => createEmployeeRecord(employee));
+    return records;
+}
+
+function createTimeInEvent(dateTime) {
+    let timeIn = {};
+    timeIn.type = "TimeIn";
+    [timeIn.date, timeIn.hour] = dateTime.split(' ');
+    timeIn.hour = parseInt(timeIn.hour,10);
+    this.timeInEvents.push(timeIn);
+    return this;
+}
+
+function createTimeOutEvent(dateTime) {
+    let timeOut = {};
+    timeOut.type = "TimeOut";
+    [timeOut.date, timeOut.hour] = dateTime.split(' ');
+    timeOut.hour = parseInt(timeOut.hour,10);
+    this.timeOutEvents.push(timeOut);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let inTime = 0;
+    let outTime = 0;
+    this.timeInEvents.forEach(event => {
+        if(event.date === date) {
+            inTime = event.hour;
+        }
+    })
+    this.timeOutEvents.forEach(event => {
+        if(event.date === date) {
+            outTime = event.hour;
+        }
+    })
+    return (outTime - inTime)/100;
+}
+
+function wagesEarnedOnDate(date) {
+    let hoursCalc = hoursWorkedOnDate.bind(this);
+    return hoursCalc(date) * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray,firstName) {
+    let foundEmp = undefined;
+    srcArray.forEach(employee => {
+        if(employee.firstName == firstName) {
+            foundEmp = employee;
+        }
+    })
+    return foundEmp;
+}
+function calculatePayroll(employees) {
+     let payRoll = 0;
+     employees.forEach(employee => {
+         let wages = allWagesFor.bind(employee)
+         payRoll = payRoll + wages();
+     })
+     return payRoll;
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
